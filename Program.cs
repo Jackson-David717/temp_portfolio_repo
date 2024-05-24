813fc7b0262bfa00d72a4c9182e4ea1f1eb12803
jackson_david717 <jackson_david717@outlook.com>
2024-05-24 14:25:20 -0500

    [Create Repository]

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+IEnumerable<string> listOfDirectories = Directory.EnumerateFiles("stores", "*.txt", SearchOption.AllDirectories);
+
+foreach (var file in listOfDirectories) {
+  Console.WriteLine(file);
+}
